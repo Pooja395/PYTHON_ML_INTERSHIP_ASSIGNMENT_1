6976c78a2eb494aab5e9828c3449791278532fae
POOJA <143282785+Pooja395@users.noreply.github.com>
2024-06-18 20:57:24 +0530

    Add files via upload

---
+++ key.py
@@ -0,0 +1,23 @@
+'''import keyword
+print(len(keyword.kwlist))
+a=input("enter the name:")
+print(a)'''
+
+
+
+str1="hello"
+str2="welcome!"
+str3=str1 + str2
+print(str3)
+
+num1=10
+print("the num is:",num1,"the type is:",type(num1))
+#typecasting in number is automatic or implicit
+#at time we may need to force typecasting so there it will be explicit 
+#implicit - automatic smaller data will be converted to a bigger datatype
+# MATH MODULE
+# math calculation 
+#alliases means nick name
+import math as mt 
+print(mt.factorial(5))
+#sequence data -> list.string and tuple 
---
+++ string.py
@@ -0,0 +1,19 @@
+'''str="hello" # single line string
+print(str)
+str1="hello this is first class\
+    kkk \ hello"
+print(str1)
+str2="heelo"\
+"heelo"
+print(str2)
+k="heelo" 
+'''
+s="hello world"
+print(s[1:5])
+print(s[1:7:2])
+print(s[-5:-1])
+print(s[-1:-5])
+print(s.find('hello'))# give the index of first letter.
+print(s.split()) # splits using seperators and default seperator is space
+z=complex(1.25)
+print(z)
